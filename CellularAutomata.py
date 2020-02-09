f275d56909039221d4550b473cec428ff8e6d2c1
asajs <asajamess@gmail.com>
2020-02-08 21:10:54 -0700

    Made a lot of speed improvements to cellularautomata

--- CellularAutomata.py
+++ CellularAutomata.py
@@ -7,8 +7,10 @@ class CellularAutomata:
         self.chance_to_stay_alive = 0.4
         self.death_limit = 3
         self.birth_limit = 4
-        self.wall = wall
-        self.empty = empty
+        self.wall_ascii = wall
+        self.empty_ascii = empty
+        self.wall_int = 1
+        self.empty_int = 0
 
     @staticmethod
     def create_grid(width, height):
@@ -64,37 +66,43 @@ class CellularAutomata:
         return new_grid
 
     def generate(self, width, height):
+        """Generate a map and return it in ascii characters"""
         automata_map = self.create_grid(width, height)
         self.initialize_grid(automata_map)
         for step in range(self.number_of_steps):
             automata_map = self.do_simulation_step(automata_map)
 
+        self.validate_map(automata_map)
         ascii_map = self.make_ascii_map(automata_map)
-        self.validate_map(ascii_map)
         return ascii_map
 
     def make_ascii_map(self, automata_map):
+        """Convert the map from 0, 1, to the chosen empty ascii character and the chosen ascii wall character"""
         ascii_map = list()
-        for row in range(len(automata_map)):
+        height_of_map = len(automata_map)
+        width_of_map = len(automata_map[0])
+        for row in range(height_of_map):
             temp_row = list()
-            for col in range(len(automata_map[0])):
+            for col in range(width_of_map):
                 if automata_map[row][col] == 1:
-                    temp_row.append(self.wall)
+                    temp_row.append(self.wall_ascii)
                 else:
-                    temp_row.append(self.empty)
+                    temp_row.append(self.empty_ascii)
             ascii_row = ''.join(temp_row)
             ascii_map.append(ascii_row)
         return ascii_map
 
-    def validate_map(self, ascii_map):
+    def validate_map(self, automata_map):
+        """Make sure that every empty spot can be visited"""
+        height_of_map = len(automata_map)
+        width_of_map = len(automata_map[0])
         unvisited = set()
         visited = set()
         local = set()
-        for _y in range(len(ascii_map)):
-            for _x in range(len(ascii_map[0])):
-                if ascii_map[_y][_x] == self.empty:
+        for _y in range(height_of_map):
+            for _x in range(width_of_map):
+                if automata_map[_y][_x] == self.empty_int:
                     unvisited.add((_y, _x))
-
 
         while len(unvisited) > 0:
             coord = unvisited.pop()
@@ -116,61 +124,90 @@ class CellularAutomata:
                 visited.add(visited_coord)
                 unvisited_coord = unvisited.pop()
                 unvisited.add(unvisited_coord)
-                self.make_paths_in_map(ascii_map, visited_coord, unvisited_coord)
+                self.make_paths_in_map(automata_map, visited_coord, unvisited_coord)
                 # This is ugly and gross, but I'm just curious... Rational: the map is different now
                 unvisited.clear()
                 visited.clear()
-                for _y in range(len(ascii_map)):
-                    for _x in range(len(ascii_map[0])):
-                        if ascii_map[_y][_x] == self.empty:
+                for _y in range(height_of_map):
+                    for _x in range(width_of_map):
+                        if automata_map[_y][_x] == self.empty_int:
                             unvisited.add((_y, _x))
 
-    def make_paths_in_map(self, ascii_map, visited_coord, unvisited_coord):
+    def make_paths_in_map(self, automata_map, visited_coord, unvisited_coord):
+        """Make holes in the walls of the map from two coordinates"""
         # first "walk" the coordinates to each other
-        visited_coord = self.greedy_best_search(visited_coord, unvisited_coord, ascii_map)
-        unvisited_coord = self.greedy_best_search(unvisited_coord, visited_coord, ascii_map)
+        unvisited_coord = self.greedy_best_search(unvisited_coord, visited_coord, automata_map)
+        visited_coord = self.greedy_best_search(visited_coord, unvisited_coord, automata_map)
         vy = visited_coord[0]
         vx = visited_coord[1]
         uy = unvisited_coord[0]
         ux = unvisited_coord[1]
+        # then delete them
         while vy != uy or vx != ux:
             if vy < uy:
                 vy += 1
-                ascii_map[vy] = ascii_map[vy][:vx] + self.empty + ascii_map[vy][vx + 1:]
             elif vy > uy:
                 vy -= 1
-                ascii_map[vy] = ascii_map[vy][:vx] + self.empty + ascii_map[vy][vx + 1:]
+
             if vx < ux:
                 vx += 1
-                ascii_map[vy] = ascii_map[vy][:vx] + self.empty + ascii_map[vy][vx + 1:]
             elif vx > ux:
                 vx -= 1
-                ascii_map[vy] = ascii_map[vy][:vx] + self.empty + ascii_map[vy][vx + 1:]
+            automata_map[vy][vx] = self.empty_int
+            self.random_walk(automata_map, vy, vx)
+
+    def random_walk(self, automata_map, vy, vx):
+        """Randomly move from a specified coordinate"""
+        height_of_map = len(automata_map)
+        width_of_map = len(automata_map[0])
+        y = vy
+        x = vx
+        number_of_steps = random.randrange(1, 3)
+        while number_of_steps > 0:
+            y = self.clamp(random.randrange(-1, 1) + y, height_of_map, 0)
+            x = self.clamp(random.randrange(-1, 1) + x, width_of_map, 0)
+            automata_map[vy][vx] = self.empty_int
+            number_of_steps -= 1
+
+    @staticmethod
+    def clamp(number, max, min):
+        """Make sure a number does not exceed its bounds"""
+        if number > max:
+            return max
+        elif number < min:
+            return min
+        return number
 
     @staticmethod
     def manhattan_distance(x, y):
+        """Return the manhattan distance"""
         return abs(x[0] - y[0]) + abs(x[1] - y[1])
 
-    def greedy_best_search(self, start, goal, ascii_map):
+    def greedy_best_search(self, start, goal, automata_map):
+        """Look for the shortest path between to points. Returns the coordinate that gets the closest"""
+        height_of_map = len(automata_map)
+        width_of_map = len(automata_map[0])
         i = 0
         frontier = []
         heapq.heappush(frontier, [0, i, start])
         best_so_far = (self.manhattan_distance(start, goal), start)
         came_from = {}
         came_from[start] = None
+        iterations_without_improvement = 0
 
         while len(frontier) > 0:
+            iterations_without_improvement += 1
             _, _, current = heapq.heappop(frontier)
 
             neighbors = list()
 
-            if current[0] - 1 > 0 and ascii_map[current[0] - 1][current[1]] == self.empty:
+            if current[0] - 1 > 0 and automata_map[current[0] - 1][current[1]] == self.empty_int:
                 neighbors.append((current[0] - 1, current[1]))
-            if current[0] + 1 < len(ascii_map) and ascii_map[current[0] + 1][current[1]] == self.empty:
+            if current[0] + 1 < height_of_map and automata_map[current[0] + 1][current[1]] == self.empty_int:
                 neighbors.append((current[0] + 1, current[1]))
-            if current[1] - 1 > 0 and ascii_map[current[0]][current[1] - 1] == self.empty:
+            if current[1] - 1 > 0 and automata_map[current[0]][current[1] - 1] == self.empty_int:
                 neighbors.append((current[0], current[1] - 1))
-            if current[1] + 1 < len(ascii_map[current[0]]) and ascii_map[current[0]][current[1] + 1] == self.empty:
+            if current[1] + 1 < width_of_map and automata_map[current[0]][current[1] + 1] == self.empty_int:
                 neighbors.append((current[0], current[1] + 1))
 
             for next in neighbors:
@@ -179,13 +216,17 @@ class CellularAutomata:
                     priority = self.manhattan_distance(next, goal)
                     if priority < best_so_far[0]:
                         best_so_far = (priority, current)
+                        iterations_without_improvement = 0
                     heapq.heappush(frontier, [priority, i, next])
                     came_from[next] = current
+            if iterations_without_improvement > 50:
+                break
 
         return best_so_far[1]
 
     @staticmethod
     def helper_for_sets(coord, unvisited, visited, local):
+        """Remove a coord from unvisited and add it to visited"""
         if coord in unvisited:
             unvisited.remove(coord)
             if coord not in visited:
